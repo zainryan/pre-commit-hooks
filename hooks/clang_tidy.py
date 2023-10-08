3580f6e2ac76d2b768a3b5aa16f5686e1ed1bf69
Zain Ruan <zainryan95@gmail.com>
2023-10-08 20:06:48 +0000

    Fix race conditions in the clang_tidy hook.

--- hooks/clang_tidy.py
+++ hooks/clang_tidy.py
@@ -5,6 +5,7 @@ import re
 import sys
 from typing import List
 import multiprocessing as mp
+import copy
 
 from hooks.utils import StaticAnalyzerCmd
 
@@ -19,21 +20,23 @@ class ClangTidyCmd(StaticAnalyzerCmd):
         self.parse_args(args)
         self.edit_in_place = "-fix" in self.args or "--fix-errors" in self.args
 
-    def tidy_file(self, filename: List[str]):
-        self.run_command([filename] + self.args)
+    def tidy_file(self, filename: str):
+        worker = copy.copy(super())
+        worker.run_command([filename] + worker.args)
         # Warnings generated aren't important.
-        self.stderr = re.sub(rb"[\d,]+ warning \S+\s+", b"", self.stderr)
-        if len(self.stderr) > 0 and "--fix-errors" in self.args:
-            self.returncode = 1
-        return self.returncode, self.stderr
+        worker.stderr = re.sub(rb"[\d,]+ warning \S+\s+", b"", worker.stderr)
+        if len(worker.stderr) > 0 and "--fix-errors" in worker.args:
+            worker.returncode = 1
+        return worker.returncode, worker.stdout, worker.stderr
 
     def run(self):
         """Run clang-tidy. If --fix-errors is passed in, then return code will be 0, even if there are errors."""
         with mp.Pool(mp.cpu_count()) as p:
             results = p.map(self.tidy_file, self.files)
-            ret_codes, stderrs = zip(*results)
+            ret_codes, stdouts, stderrs = zip(*results)
 
             self.returncode = max(ret_codes)
+            self.stdout = b"\n".join(stdouts)
             self.stderr = b"\n".join(stderrs)
             self.exit_on_error()
 
@@ -43,3 +46,4 @@ def main(argv: List[str] = sys.argv):
 
 if __name__ == "__main__":
     main()
+
